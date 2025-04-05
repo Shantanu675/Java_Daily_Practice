9f94a5d0d9672116591fbca8125852f1d3645fa9
Shantanu675 <shantanudanej05@gmail.com>
2025-04-06 01:44:08 +0530

    doubly, circular linkedList implementation

--- src/LinkedList/LL.java
+++ src/LinkedList/LL.java
@@ -323,5 +323,76 @@ public class LL {
 
         return merge(left, right);
     }
+
+    //Bubble sort
+    public void bubbleSort() {
+        bubbleSort(size - 1, 0);
+    }
+
+    private void bubbleSort(int row, int col) {
+        if (row == 0) {
+            return;
+        }
+
+        if (col < row) {
+            Node first = get(col);
+            Node second = get(col + 1);
+
+            if (first.value > second.value) {
+                // swap
+                if (first == head) {
+                    head = second;
+                    first.next = second.next;
+                    second.next = first;
+                } else if (second == tail) {
+                    Node prev = get(col - 1);
+                    prev.next = second;
+                    tail = first;
+                    first.next = null;
+                    second.next = tail;
+                } else {
+                    Node prev = get(col - 1);
+                    prev.next = second;
+                    first.next = second.next;
+                    second.next = first;
+                }
+            }
+            bubbleSort(row, col + 1);
+        } else {
+            bubbleSort(row - 1, 0);
+        }
+    }
+
+    // recursive reverse LL
+    public void reverse(Node node) {
+        if(node == tail){
+            head = tail;
+            return;
+        }
+        reverse(node.next);
+        tail.next = node;
+        tail = node;
+        tail.next = null;
+    }
+
+    // in-place reverse
+    public Node reverseInPlace(Node node) {
+        if(head == null || head.next == null){
+            return head;
+        }
+
+        Node prev = null;
+        Node curr = head;
+
+        while(curr != null){
+            Node next = curr.next;
+
+            curr.next = prev;
+
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
 }
 
