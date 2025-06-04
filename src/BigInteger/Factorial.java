b585ff88e00e1d2c9affc117d01c04e89e4702eb
Shantanu675 <shantanudanej05@gmail.com>
2025-06-04 22:34:42 +0530

    some stack problems add

--- src/BigInteger/Factorial.java
+++ src/BigInteger/Factorial.java
@@ -6,7 +6,7 @@ public class Factorial {
     public static void main(String[] args) {
         BigInteger ans = new BigInteger("1");
 
-        for(int i = 1; i <= 100; i++){
+        for(int i = 1; i <= 13; i++){
             ans = ans.multiply(BigInteger.valueOf(i));
         }
 
